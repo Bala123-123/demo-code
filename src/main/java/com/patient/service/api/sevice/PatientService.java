ceaf7d1b8a8a717a607d88ec88d60d20bd1e3fc9
bala <balapotla959@gmail.com>
2026-09-22 21:59:57 +0530

    added another statement

--- src/main/java/com/patient/service/api/sevice/PatientService.java
+++ src/main/java/com/patient/service/api/sevice/PatientService.java
@@ -44,7 +44,7 @@ public class PatientService {
     public void deletePatient(Long id) {
 
         System.out.println("balu");
-
+        System.out.println("priyanshi");
         patientRepository.deleteById(id);
     }
 }
